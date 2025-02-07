223e990d65dd9a7918786c7c4c49e06da2f77ecb
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2025-02-07 18:07:39 +0100

    fix: img-masterProf

--- app/(route)/contact/page.tsx
+++ app/(route)/contact/page.tsx
@@ -12,7 +12,13 @@ export default function Contact() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = { email, phone, firstName, lastName, message };
+    const data = {
+      email,
+      phone,
+      firstName,
+      lastName,
+      message,
+    };
 
     console.log("Отправляемые данные:", data);
 
@@ -56,97 +62,84 @@ export default function Contact() {
   return (
     <div className="mt-7 px-4 sm:px-0">
       <form
-        className="max-w-lg mx-auto bg-white p-6 rounded-lg shadow-lg"
+        className="container max-w-lg mx-auto bg-white p-6 rounded-lg shadow-lg space-y-3 sm:space-y-4"
         onSubmit={handleSubmit}
+        style={{ backgroundColor: "transparent" }}
       >
-        <h1 className="text-green-600 text-xl font-extrabold sm:text-3xl text-center">
+        <h1 className="text-green-600 text-xl font-extrabold sm:text-5xl text-center">
           Kontaktieren Sie uns
-          <strong className="block font-extrabold text-green-900">
-            bei Fragen.
-          </strong>
+          <strong className="block font-extrabold text-green-900"> bei Fragen. </strong>
         </h1>
 
-        <div className="mt-5 space-y-4">
-          <div className="text-green-700">
-            <label htmlFor="frm-email" className="block text-sm font-medium">
-              Email
-            </label>
-            <input
-              id="frm-email"
-              type="email"
-              name="email"
-              autoComplete="email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-            />
-          </div>
+        <div className="email block text-green-700">
+          <label htmlFor="frm-email">Email</label>
+          <input
+            id="frm-email"
+            type="email"
+            name="email"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+          />
+        </div>
 
+        <div className="block phone text-green-700">
+          <label htmlFor="frm-phone">Telefonnummer:</label>
+          <input
+            id="frm-phone"
+            type="text"
+            name="phone"
+            autoComplete="tel"
+            required
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+          />
+        </div>
+
+        <div className="name grid sm:grid-cols-2 gap-4">
           <div className="text-green-700">
-            <label htmlFor="frm-phone" className="block text-sm font-medium">
-              Telefonnummer
-            </label>
+            <label htmlFor="frm-first">Name:</label>
             <input
-              id="frm-phone"
+              id="frm-first"
               type="text"
-              name="phone"
-              autoComplete="tel"
+              name="first"
+              autoComplete="given-name"
               required
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
               className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
-
-          <div className="flex flex-col sm:flex-row gap-4">
-            <div className="text-green-700 w-full">
-              <label htmlFor="frm-first" className="block text-sm font-medium">
-                Name
-              </label>
-              <input
-                id="frm-first"
-                type="text"
-                name="first"
-                autoComplete="given-name"
-                required
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-              />
-            </div>
-
-            <div className="text-green-700 w-full">
-              <label htmlFor="frm-last" className="block text-sm font-medium">
-                Nachname
-              </label>
-              <input
-                id="frm-last"
-                type="text"
-                name="last"
-                autoComplete="family-name"
-                required
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
-              />
-            </div>
-          </div>
-
           <div className="text-green-700">
-            <label htmlFor="frm-message" className="block text-sm font-medium">
-              Nachricht
-            </label>
-            <textarea
-              id="frm-message"
-              name="message"
+            <label htmlFor="frm-last">Nachname:</label>
+            <input
+              id="frm-last"
+              type="text"
+              name="last"
+              autoComplete="family-name"
               required
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 h-32"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
+        </div>
 
+        <div className="message block text-green-700">
+          <label htmlFor="frm-message">Nachricht</label>
+          <textarea
+            id="frm-message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 h-32"
+          />
+        </div>
+
+        <div className="button block white">
           <button
             type="submit"
             className="w-full bg-green-700 text-white font-bold py-3 px-4 rounded-lg cursor-pointer hover:bg-green-800 transition text-lg"
