3e8372c27e40ed70e158f1922965a7cf2a15c664
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2025-02-15 10:47:02 +0100

    Fix

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -46,7 +46,7 @@ function Hero() {
                 className="mt-10 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300"
                 style={{ backgroundColor: "#006400", color: "#ffffff" }}
               >
-                Jetzt Erkunden
+                Meister in der Nähe
               </Button>
             </Link>
           </div>
